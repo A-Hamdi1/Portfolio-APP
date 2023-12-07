6315f9090271b51caed1861b643d84d5f2b57819
A-Hamdi1 <akramhamdi299@gmail.com>
2023-12-07 01:40:04 +0100

    07-12-2023

--- lib/constant/Colors.dart
+++ lib/constant/Colors.dart
@@ -5,11 +5,9 @@ class TColors {
 
   static const Color primary = Color(0xFF4b68ff);
   static const Color secondary = Color(0xFFFFE24B);
-  // Background Colors
   static const Color light = Color(0xFFF6F6F6);
   static const Color dark = Color(0xFF272727);
 
-  // Neutral Shades
   static const Color black = Color(0xFF232323);
   static const Color grey = Color(0xFF0E0E0);
   static const Color white = Color(0xFFFFFFFF);
